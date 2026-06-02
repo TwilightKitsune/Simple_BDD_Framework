795b9dce532347167e41bc2e081163e12aa57617
TwilightKitsune <93722369+TwilightKitsune@users.noreply.github.com.>
2026-06-02 18:04:21 +0400

    Fix: remove WebDriverManager from java code

--- autotest-web/src/main/java/hooks/WebHooks.java
+++ autotest-web/src/main/java/hooks/WebHooks.java
@@ -5,7 +5,6 @@ import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.aeonbits.owner.ConfigFactory;
 import ru.lanit.at.utils.ErrorMessage;
@@ -29,13 +28,10 @@ public class WebHooks {
 
         switch (cfg.webDriverBrowserName()) {
             case "chrome":
-                WebDriverManager.chromedriver().setup();
                 break;
             case "firefox":
-                WebDriverManager.firefoxdriver().setup();
                 break;
             case "edge":
-                WebDriverManager.edgedriver().setup();
                 break;
             default: {
                 throw new IllegalArgumentException(String.format(ErrorMessage.BROWSER_NOT_SUPPORTED, cfg.webDriverBrowserName()));
